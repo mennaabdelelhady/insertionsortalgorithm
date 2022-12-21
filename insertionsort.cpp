064fc6c250ec34465e8208d082e1581f8016c768
mennaabdelelhady <77082822+mennaabdelelhady@users.noreply.github.com>
2022-12-21 16:55:29 +0200

    Add files via upload

---
+++ insertionsort.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+void insertionSort(int arr[], int n)
+{
+	int i, key, j;
+	for (i = 1; i < n; i++)
+	{
+		key = arr[i];
+		j = i - 1;
+
+		while (j >= 0 && arr[j] > key)
+		{
+			arr[j + 1] = arr[j];
+			j = j - 1;
+		}
+		arr[j + 1] = key;
+	}
+}
+
+int main()
+{   int N;cin>>N;
+	int arr[N];
+	for(int i=0;i<N;i++){
+        cin>>arr[i];
+	}
+
+
+	insertionSort(arr, N);
+    for(int i=0;i<N;i++){
+        cout<<arr[i]<<" ";
+    }
+	return 0;
+}
+
